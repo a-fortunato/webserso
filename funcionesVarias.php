1401626ffba831d101085a72a56754437a31111f
Anggella Fortunato <anggyfortun@gmail.com>
2014-05-10 13:43:27 +0200

    index, funciones e imágenes
    Limpiado de index, añadir cómo crear y utilizar clases en
    funcionesVarias, actualización de imagen, añadido de txt para lectura de
    imágenes.

--- funcionesVarias.php
+++ funcionesVarias.php
@@ -1,18 +1,35 @@
+<!-- Funciones varias. Objetivo final: leer imágenes desde .txt y mostrarlas en la web desde donde se llamen -->
+
 <?php
 /*** Leer un texto línea a línea. ***/
 
-$file = fopen("1.txt", "r") or exit("Unable to open file!");
-//Output a line of the file until the end is reached
-while(!feof($file))
-{
-echo fgets($file). "<br />";
-}
-fclose($file);
+	$file = fopen("1.txt", "r") or exit("Unable to open file!");
+	//Output a line of the file until the end is reached
+	while(!feof($file)){
+		echo fgets($file). "<br />";
+	}
+	fclose($file);
 ?>
 
 <?php
 /*** Mostrar la imagen de la variable figura en la carpeta destino. ***/
 
-	echo '<img src="images/'.$destino'/'.$figura.'.jpg" />';
+	echo '<img src="images/'.$destino.'/'.$figura.'.jpg" />';
 	// de forma que $figura="cuadro"; mostraría la imagen cuadro.jpg
+?>
+
+
+<?php // claseImagen.php
+	class imagen{
+		public static function obtenerImagen($destino){
+		
+		}
+	}
+?>
+
+
+<?php // Sitio desde el cual llamamos a la clase imagen y sus métodos
+	include ("claseImagen.php");
+	
+	imagen::obtenerImagen(&dest);
 ?>
